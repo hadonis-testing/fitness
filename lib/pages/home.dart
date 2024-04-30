17b77086d9e058f0b7f3c26d2fcb57c5537dd6a7
Minh Vương <hardingadonis@gmail.com>
2024-04-30 11:52:48 +0700

    optimize search bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,11 +11,11 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: [
           Container(
-            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+            margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
             decoration: BoxDecoration(
               boxShadow: [
                 BoxShadow(
-                  color: Color(0xff1D1617).withOpacity(0.11),
+                  color: const Color(0xff1D1617).withOpacity(0.11),
                   blurRadius: 40,
                   spreadRadius: 0.0,
                 ),
@@ -25,30 +25,30 @@ class HomePage extends StatelessWidget {
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Colors.white,
-                contentPadding: EdgeInsets.all(15),
+                contentPadding: const EdgeInsets.all(15),
                 hintText: 'Search Pancake',
-                hintStyle: TextStyle(
+                hintStyle: const TextStyle(
                   color: Color(0xffDDDADA),
                   fontSize: 14,
                 ),
                 prefixIcon: Padding(
-                  padding: EdgeInsets.all(12),
+                  padding: const EdgeInsets.all(12),
                   child: SvgPicture.asset('assets/icons/Search.svg'),
                 ),
-                suffixIcon: Container(
-                  width: 100,
+                suffixIcon: SizedBox(
+                  width: 60,
                   child: IntrinsicHeight(
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        VerticalDivider(
+                        const VerticalDivider(
                           color: Colors.black,
                           indent: 10,
                           endIndent: 10,
                           thickness: 0.1,
                         ),
                         Padding(
-                          padding: EdgeInsets.all(8),
+                          padding: const EdgeInsets.all(8),
                           child: SvgPicture.asset('assets/icons/Filter.svg'),
                         ),
                       ],
